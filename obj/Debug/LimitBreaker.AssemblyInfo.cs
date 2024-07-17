48f9fc4b97c47fd457caa9b2f77e28d685b3e964
Draexico <kostajorgji@hotmail.com>
2024-07-17 17:45:29 -0400

    XD

--- obj/Debug/LimitBreaker.AssemblyInfo.cs
+++ obj/Debug/LimitBreaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LimitBreaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98de63b22900ef21fb5cd677b411ac0c9fcc3fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00c8475fd2d936dd6de26b9f4813783cd29ce3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LimitBreaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LimitBreaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
